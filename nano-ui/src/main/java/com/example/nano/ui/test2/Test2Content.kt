c1f5a4ca3ae7ca30c9ca4b5cc42d4f36eaf6c87e
nanoyatsu <34255833+nanoyatsu@users.noreply.github.com.>
2025-03-09 01:24:09 +0900

    add Test2Content

---
+++ nano-ui/src/main/java/com/example/nano/ui/test2/Test2Content.kt
@@ -0,0 +1,30 @@
+package com.example.nano.ui.test2
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Test2Content() {
+    Surface(
+        modifier = Modifier
+            .safeContentPadding()
+            .fillMaxSize()
+    ) {
+        Column {
+            Text("Test2Content")
+            Text("★　☆　★")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Test2ContentPreview() {
+    Test2Content()
+}
